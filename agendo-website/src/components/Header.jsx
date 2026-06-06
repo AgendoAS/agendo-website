6e9baeae4bb9175c34136653d94be9179c8c1245
AgendoAS <contato@agendoas.com>
2026-06-06 19:41:01 -0300

    Update Header.jsx

--- agendo-website/src/components/Header.jsx
+++ agendo-website/src/components/Header.jsx
@@ -19,8 +19,7 @@ export default function Header() {
       <nav className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
         {/* Logo */}
       <Link to="/" className="flex items-center">
-          <img src="/AGENDO.png" alt="AGENDO Logo" className="h-16 w-auto" />
-        </Link>        {/* Menu Desktop */}
+<img src="/public/AGENDO.png" alt="AGENDO Logo" className="h-16 w-auto" />        </Link>        {/* Menu Desktop */}
         <div className="hidden md:flex items-center gap-8">
           {navLinks.map((link) => (
             <Link
